4bdae811f57db7072f4ad8e48828a4bd4f9c3710
Matthew Keifer Streich <streich@linprog3.cs.fsu.edu>
2014-12-04 22:20:53 -0500

    Working msd.h

--- msd.h
+++ msd.h
@@ -17,8 +17,7 @@ public:
   {
   }
 
-  template <class Predicate>
-  void Sort(fsu::Vector< fsu::Vector<Char> > & msd_data, Predicate& predicate);
+  void Sort(fsu::Vector< fsu::Vector<Char> > & msd_data);
 
 private:
   int charAt(fsu::Vector<Char> s, size_t pos);
@@ -29,8 +28,7 @@ private:
 };
 
 template <typename T>
-template <class Predicate>
-void MSD<T>::Sort(fsu::Vector< fsu::Vector<Char> > & msd_data, Predicate& predicate)
+void MSD<T>::Sort(fsu::Vector< fsu::Vector<Char> > & msd_data)
 {
   (void)msd_data;
   fsu::Vector<fsu::Vector<Char> > aux( msd_data.Size());
@@ -41,22 +39,27 @@ void MSD<T>::Sort(fsu::Vector< fsu::Vector<Char> > & msd_data, Predicate& predic
 template <typename T>
 void MSD<T>::privateSort( fsu::Vector< fsu::Vector<Char> > & str, fsu::Vector< fsu::Vector<Char> > & aux,
                           size_t lo, size_t hi, size_t d){
-  if(hi <= lo) return;
+ 
+ if(hi <= lo) return;
+ //Initialize a fresh count vector every call
   fsu::Vector<size_t> Count;
  for(size_t j = 0; j <= R+2; j++) Count.PushBack(0); 
- for(size_t j = lo; j <= hi; j++){
 
+ //Get counts of all characters
+ for(size_t j = lo; j <= hi; j++)
     Count[charAt(str[j],d)+2]++;
- }
+ //Track total character count
  for(size_t r = 0; r < R+1; r++)
     Count[r+1] += Count[r];
+ //Rearrange strings in aux based on total character count
  for(size_t i = lo; i <= hi; i++)
     aux[Count[charAt(str[i],d)+1]++] = str[i];
+ //Put strings with correct index back into original vector
  for(size_t i = lo; i <= hi; i++)
     str[i] = aux[i - lo];
+ //Recurse through alphabet on next letter
  for(size_t r = 0; r < R; r++){
-  if(lo + Count[r+1] < 1) continue;
-  //std::cout << lo + Count[r] << " = lo " << lo + Count[r+1] -1 << " = hi" << d << std::endl;
+  if(lo + Count[r+1] < 1) continue; //Since using -1 w/ size_t should check for underflow
   privateSort( str, aux, lo + Count[r] , lo + Count[r+1] - 1 , d+1);
  }
 }
